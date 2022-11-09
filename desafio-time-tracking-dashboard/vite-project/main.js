23d1a652efbd7d507a205195824b86ef4f80ef20
mateusdev7 <mateusdev1337@gmail.com>
2022-11-09 18:08:54 -0300

    Parte inicial

--- desafio-time-tracking-dashboard/vite-project/main.js
+++ desafio-time-tracking-dashboard/vite-project/main.js
@@ -1,2 +1,48 @@
+const containerInfo = document.querySelector(".container-info");
 
+const createCardWekly = (valueBox, name, current, previous) => {
+  const boxContainer = document.createElement('div');
+  boxContainer.className = `box-container box-container${valueBox}`;
+    const boxBg = document.createElement('div');
+    boxBg.className = 'box-bg';
+    const imgIcon = document.createElement('img');
+    imgIcon.className = 'icon';
+    imgIcon.src = `/assets/icon-${name}.svg`;
+    const boxContent = document.createElement('div');
+    boxContent.className = 'box-content'
+      const boxTitle = document.createElement('div');
+      boxTitle.className = 'box-title';
+        const h3BoxTitle = document.createElement('h3')
+        h3BoxTitle.textContent = name;
+        const imgBoxTitle = document.createElement('img');
+        imgBoxTitle.src = '/assets/icon-ellipsis.svg';
+      const boxTitleInfo = document.createElement('div');
+      boxTitleInfo.className = 'box-title-info';
+        const h1BoxTitleInfo = document.createElement('h1');
+        h1BoxTitleInfo.textContent = `${current}hrs`
+        const pBoxTitleInfo = document.createElement('p');
+        pBoxTitleInfo.textContent = `Last Week - ${previous}hrs`
+      
+      boxContainer.appendChild(boxBg);
+      boxContainer.appendChild(imgIcon);
+      boxContainer.appendChild(boxContent);
+      boxContent.appendChild(boxTitle);
+      boxTitle.appendChild(h3BoxTitle);
+      boxTitle.appendChild(imgBoxTitle);
+      boxContent.appendChild(boxTitleInfo);
+      boxTitleInfo.appendChild(h1BoxTitleInfo);
+      boxTitleInfo.appendChild(pBoxTitleInfo);
 
+      return boxContainer;
+}
+
+async function getData() {
+  const response = await fetch('./data.json');
+  const json = await response.json();
+  console.log(json);
+  json.forEach((item, index) => {
+    console.log(containerInfo.appendChild(createCardWekly(index + 1, item.title, item.timeframes.weekly.current, item.timeframes.weekly.previous)))
+  })
+}
+
+getData()
