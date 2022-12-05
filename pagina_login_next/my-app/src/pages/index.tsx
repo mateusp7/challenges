ed7c7430258b846fb633f7bc9e484d7fcd89c89c
mateusdev7 <mateusdev1337@gmail.com>
2022-12-05 13:06:54 -0300

    Cores dinamicas

--- pagina_login_next/my-app/src/pages/index.tsx
+++ pagina_login_next/my-app/src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Input from "../components/Input";
 import React from "react";
+import Swal from "sweetalert2";
+import "animate.css";
 
 export default function Home() {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [remember, setRemember] = React.useState<any | null>([]);
+
+  const colors = {
+    primaryColor: "#AFBE8F",
+    secondaryColor: "#7D8570",
+    tertiaryColor: "#646F58",
+    fullWhite: "#FFFFFF",
+  };
 
   function handleChange({ target }: React.ChangeEvent<HTMLInputElement>) {
     if (target.checked) {
@@ -16,6 +25,31 @@ export default function Home() {
 
   function checkRemember(rememberValue: string) {
     return remember.includes(rememberValue);
+  }
+
+  function handleSubmit(event: Event | React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (username == "" || password == "") {
+      Swal.fire({
+        title: "Campos Obrigatórios Vazios!",
+        text: "Por favor, preencha os campos obrigatórios",
+        icon: "error",
+        confirmButtonText: "Confirmar",
+        showClass: {
+          popup: "animate__animated animate__fadeInDown",
+        },
+        hideClass: {
+          popup: "animate__animated animate__fadeOutUp",
+        },
+      });
+    } else {
+      Swal.fire({
+        title: "Cadastro Realizado",
+        text: "Parabéns",
+        icon: "success",
+        confirmButtonText: "Confirmar",
+      });
+    }
   }
 
   return (
@@ -31,7 +65,7 @@ export default function Home() {
             <img src="/img/twitter-icon.svg" />
           </div>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Input
             type="text"
             id="username"
@@ -48,10 +82,16 @@ export default function Home() {
             setValue={setPassword}
             placeholder="Password"
           />
+          <button
+            type="submit"
+            className="w-full px-4 py-3 mb-4 font-bold text-center text-white rounded-3xl"
+            style={{
+              background: `linear-gradient(to right, ${colors.primaryColor}, ${colors.secondaryColor}`,
+            }}
+          >
+            Sign in
+          </button>
         </form>
-        <button className="w-full px-4 py-3 text-center bg-gradient-to-r from-[#E46263] to-[#E46181] rounded-3xl text-white font-bold mb-4">
-          Sign in
-        </button>
         <div className="flex flex-row justify-between">
           <div className="flex flex-row items-center gap-2">
             <input
@@ -61,14 +101,23 @@ export default function Home() {
               value="Remember me"
               checked={checkRemember("Remember me")}
               onChange={handleChange}
-              className="w-4 h-4 m-0 bg-white border-2 rounded appearance-none checked:bg-[#E46263] border-slate-500 checked:border-[#E46263] cursor-pointer"
+              className="w-4 h-4 m-0 bg-white border-2 rounded appearance-none cursor-pointer border-slate-500"
+              style={
+                remember.length > 0
+                  ? {
+                      background: colors.primaryColor,
+                      borderColor: colors.primaryColor,
+                    }
+                  : { background: "#ffffff" }
+              }
             />
             <label
               htmlFor="remember"
-              className={
+              className="cursor-pointer"
+              style={
                 remember.length > 0
-                  ? "text-[#E46263] cursor-pointer"
-                  : "text-black cursor-pointer"
+                  ? { color: colors.primaryColor }
+                  : { color: colors.tertiaryColor }
               }
             >
               Remember me
@@ -79,7 +128,12 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="p-10 w-full bg-gradient-to-r from-[#E46263] to-[#E46181] self-stretch text-center flex flex-col items-center justify-center gap-3">
+      <div
+        className="flex flex-col items-center self-stretch justify-center w-full gap-3 p-10 text-center"
+        style={{
+          background: `linear-gradient(to left, ${colors.primaryColor}, ${colors.secondaryColor}`,
+        }}
+      >
         <h1 className="text-3xl font-bold text-white">Welcome to login</h1>
         <p className="font-bold text-white">Don't have an account?</p>
         <button className="px-6 py-2 mt-4 font-bold text-white border-2 rounded-3xl">
